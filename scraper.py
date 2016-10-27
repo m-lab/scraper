412e95c72e0e59469d479f27e56acc0eefaf1da1
Peter Boothe <pboothe@google.com>
2016-10-27 15:07:41 +0000

    Scrape mlab nodes.
    Initial code to scrape mlab nodes.  Doesn't (yet) upload the data or interact with the spreadsheet, but enough code has been written that a review is a good idea.

---
+++ scraper.py
@@ -0,0 +1,306 @@
+#!/usr/bin/env python
+# Copyright 2016 Scraper Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Download all new data from an MLab node, then upload what can be uploaded.
+
+This is a single-shot program to download data from an MLab node and then
+upload it to Google Cloud Storage.  It is expected that this program will be
+called every hour (or so) by a cron job, and that there will be many such cron
+jobs running in a whole fleet of containers run by Google Container Engine.
+"""
+
+import argparse
+import atexit
+import datetime
+import logging
+import os
+import subprocess
+import sys
+import tempfile
+
+
+def acquire_lock_or_die(lockfile):
+  """Prevents long-running downloads from being stepped on.
+
+  Create the lockfile and write the pid to that file.  If the lockfile already
+  exists, exit.
+
+  Args:
+    lockfile: the filename of the file to create
+  """
+  # This is bad locking. But we're looking to protect binaries
+  # that are running hours apart from one another, not two
+  # binaries which are racing for the same file at startup.
+  assert_msg = ("Lockfile %s is present. Old job is likely still running. "
+                "Aborting." % lockfile)
+  assert not os.path.exists(lockfile), assert_msg
+  with file(lockfile, 'w') as lockfile:
+    print >>lockfile, 'PID of scraper is', os.getpid()
+
+
+def parse_cmdline(args):
+  """Parse the commandline arguments.
+
+  Args:
+    args: the command-line arguments, minus the name of the binary
+
+  Returns:
+    the results of ArgumentParser.parse_args
+  """
+  parser = argparse.ArgumentParser(
+      description='Scrape a single experiment at a site, upload the results '
+                  'if enough time has passed.')
+  parser.add_argument('--rsync_host', metavar='HOST', type=str, nargs=1,
+                      required=True, help='The host to connect to over rsync')
+  parser.add_argument('--lockfile_dir', metavar='DIR', type=str, nargs=1,
+                      required=True,
+                      help='The the directory for lockfiles to prevent old '
+                           'jobs and new jobs from running simultaneously')
+  parser.add_argument('--rsync_module', metavar='MODULE', type=str, nargs=1,
+                      required=True,
+                      help='The rsync module to connect to on the server')
+  parser.add_argument('--data_dir', metavar='DIR', type=str, nargs=1,
+                      required=True,
+                      help='The directory under which to save the data')
+  parser.add_argument('--rsync_binary', metavar='RSYNC', type=str, nargs=1,
+                      default=['/usr/bin/rsync'], required=False,
+                      help='The location of the rsync binary '
+                           '(defaults to /usr/bin/rsync)')
+  parser.add_argument('--rsync_port', metavar='PORT', type=int, nargs=1,
+                      default=7999, required=False,
+                      help='The port on which the rsync server runs '
+                           '(defaults to 7999)')
+  parser.add_argument('--spreadsheet', metavar='URL', type=str, nargs=1,
+                      required=True,
+                      help='The google doc ID of the spreadsheet used to sync '
+                           'download information with the nodes.')
+  return parser.parse_args(args)
+
+
+def list_rsync_files(rsync_binary, rsync_url, destination):
+  """Get a list of all files in the rsync module on the server.
+
+  Lists all the files we might wish to download from the server. Be
+  careful with the filenames this command returns, because they might
+  not be safe for shell interpretation. Therefore, make sure that they
+  are never interpreted by a shell. In order to ensure that, we need
+  to have the full authoritative path to the rsync binary, which is the
+  thing we pass in here.  If something goes wrong with the download, exit.
+
+  Args:
+    rsync_binary: the full path location of rsync
+    rsync_url: the rsync:// url to download the list from
+    destination: the directory on the local host to put the files
+
+  Returns:
+    a list of filenames
+  """
+  try:
+    logging.info("rsync file list discovery from %s to %s", rsync_url,
+                 destination)
+    command = [rsync_binary, rsync_url, destination, '--list-only', '-r']
+    logging.info("Listing files on server with the command: %s", command)
+    lines = subprocess.check_output(command).splitlines()
+    files = []
+    for line in lines:
+      chunks = line.split(None, 4)  # None is a special whitespace arg for split
+      if len(chunks) != 5:
+        logging.error("Bad line in output: %s", line)
+        continue
+      files.append(chunks[4])
+    return files
+  except subprocess.CalledProcessError as error:
+    logging.error('rsync file listing failed: %s', str(error))
+    sys.exit(1)
+
+
+def get_progress_from_spreadsheet(spreadsheet, rsync_host, rsync_module):  # pragma: no cover
+  # TODO(pboothe)
+  return datetime.datetime(2016, 10, 15).date()
+
+
+def remove_older_files(date, files):
+  """Creates a new list with all files at least as old as `date` removed.
+
+  Args:
+    date: the date of the last day to remove from consideration
+    files: the list of filenames
+
+  Returns:
+    a filtered list of filenames
+  """
+  filtered = []
+  for fname in files:
+    if fname.count('/') < 3:
+      logging.info("Ignoring %s on the assumption it is a directory", fname)
+      continue
+    year, month, day, _ = fname.split('/', 3)
+    if not (year.isdigit() and month.isdigit() and day.isdigit()):
+      logging.error("Bad filename. Was supposed to be YYYY/MM/DD, but was %s",
+                    fname)
+      continue
+    # Pass in a radix to guard against zero-padded 8 and 9
+    if datetime.date(int(year, 10), int(month, 10), int(day, 10)) > date:
+      filtered.append(fname)
+  return filtered
+
+
+def download_files(rsync_binary, rsync_url, files, destination):
+  """Downloads the files from the server.
+
+  The filenames may not be safe for shell interpretation, so make sure
+  they are never interpreted by a shell.  If something goes wrong with
+  the download, exit.
+
+  Args:
+    rsync_binary: The full path to `rsync`
+    rsync_url: The url from which to retrieve the files
+    files: a list of filenames to retrieve
+    distination: the directory on the local host to put the files
+  """
+  if not files:
+    logging.warning('No files to be downloaded from %s', rsync_url)
+    return
+  # Rsync all the files that are new enough for us to care about
+  with tempfile.NamedTemporaryFile() as temp:
+    # Write the list of files to a tempfile, so as not to have to worry about
+    # too-long command lines full of filenames.
+    for fname in files:
+      print >>temp, fname
+    temp.flush()
+    # Download all the files.
+    try:
+      logging.info("Downloading %d files", len(files))
+      command = [rsync_binary, '--files-from', temp.name, rsync_url,
+                 destination]
+      subprocess.check_call(command)
+    except subprocess.CalledProcessError as error:
+      logging.error('rsync download failed: %s', str(error))
+      sys.exit(1)
+    logging.info('sync completed successfully from %s', rsync_url)
+
+
+def max_new_high_water_mark():
+  """The most recent date that we could consider "old enough" to upload.
+
+  8 hours after midnight, we will assume that no tests from the previous
+  day could possibly have failed to be written to disk.  So this should
+  always be either yesterday, or the day before, depending on how late
+  in the day it is.
+  """
+  return (datetime.datetime.utcnow() -
+          datetime.timedelta(days=1, hours=8)).date()
+
+
+def find_all_days_to_upload(localdir, high_water_mark):
+  """Find all the days that are eligible to be uploaded.
+
+  Search through localdir, trying to find all the data that is from a day that
+  is old enough to be uploaded.
+
+  Args:
+    localdir: the local directory containing all the data
+    high_water_mark: the most recent day that is eligible to be uploaded
+
+  Yields:
+    a sequence of days that exist on the localhost and are old enough to be
+    uploaded.
+  """
+  for year in filter(str.isdigit, os.listdir(localdir)):
+    year_dir = os.path.join(localdir, year)
+    if not os.path.isdir(year_dir):
+      continue
+    for month in filter(str.isdigit, os.listdir(year_dir)):
+      month_dir = os.path.join(localdir, year, month)
+      if not os.path.isdir(month_dir):
+        continue
+      for day in filter(str.isdigit, os.listdir(month_dir)):
+        date_dir = os.path.join(localdir, year, month, day)
+        if not os.path.isdir(date_dir):
+          continue
+        # Make sure to specify radix 10 to prevent an octal interpretation
+        # of 0-padded single digits 08 and 09.
+        try:
+          date = datetime.date(year=int(year, 10),
+                               month=int(month, 10),
+                               day=int(day, 10))
+          if date <= high_water_mark:
+            yield date
+        except ValueError as v:
+          logging.error("Bad directory that looks like a day: %s", v)
+
+
+def create_tarball(directory, day):  # pragma: no cover
+  # TODO(pboothe)
+  return '/tmp/doesnotexist'
+
+def upload_tarball(tgz_filename):  # pragma: no cover
+  # TODO(pboothe)
+  pass
+
+def update_high_water_mark(spreadsheet, rsync_host, rsync_module, day):  # pragma: no cover
+  # TODO(pboothe)
+  pass
+
+def remove_datafiles(directory, day, tgz_filename):  # pragma: no cover
+  # TODO(pboothe)
+  pass
+
+def main():  # pragma: no cover
+  # TODO(pboothe) end-to-end tests
+  args = parse_cmdline(sys.argv[1:])
+
+  # Ensure that old long-lasting downloads don't get clobbered by new ones.
+  lockfile = os.path.join(
+      args.lockfile_dir[0],
+      '{}_{}.lock'.format(args.rsync_host[0], args.rsync_module[0]))
+  acquire_lock_or_die(lockfile)
+  atexit.register(os.remove, lockfile)
+
+  # If the destination directory does not exist, make it exist.
+  destination = os.path.join(args.data_dir[0], args.rsync_host[0],
+                             args.rsync_module[0])
+  if not os.path.isdir(destination):
+    os.makedirs(destination)
+
+  # Get the file list and then the files from the server.
+  rsync_url = 'rsync://{}:{}/{}/'.format(args.rsync_host[0], args.rsync_port,
+                                         args.rsync_module[0])
+  files = list_rsync_files(args.rsync_binary[0], rsync_url, destination)
+  progress_level = get_progress_from_spreadsheet(args.spreadsheet[0],
+                                                 args.rsync_host[0],
+                                                 args.rsync_module[0])
+  files = remove_older_files(progress_level, files)
+  download_files(args.rsync_binary[0], rsync_url, files, destination)
+
+  # Tar up what we have for each un-uploaded day that is sufficiently in the
+  # past (up to and including the new high water mark), upload what we have,
+  # and delete the local copies of all successfully-uploaded data.
+  new_high_water_mark = max_new_high_water_mark()
+  for day in find_all_days_to_upload(destination, new_high_water_mark):
+    tgz_filename = create_tarball(destination, day)
+    upload_tarball(tgz_filename)
+    update_high_water_mark(args.spreadsheet[0],
+                           args.rsync_host[0],
+                           args.rsync_module[0],
+                           day)
+    remove_datafiles(destination, day, tgz_filename)
+
+
+if __name__ == '__main__':  # pragma: no cover
+  logging.basicConfig(
+      level=logging.INFO,
+      format='[%(asctime)s %(levelname)s %(filename)s:%(lineno)d] %(message)s')
+  main()
