2573c29440ca1b8e544d498b50ddee246021d5d2
Peter Boothe <pboothe@pboothe2.nyc.corp.google.com>
2017-05-23 10:35:38 -0400

    More path name fixes

--- k8s/fill_in_templates.sh
+++ k8s/fill_in_templates.sh
@@ -8,22 +8,25 @@
 # end up with strings like "{{IMAGE" in your file without a closing "}}", then
 # your version of python does not have the bugfix.
 
-mkdir -p claims
-mkdir -p deployment
-
 USAGE="$0 <pattern> <storage_size>"
 PATTERN=${1:?Please provide a pattern for mlabconfig: $USAGE}
 GIGABYTES=${2:?Please give an integer number of gigabytes: $USAGE}
 
+# claims/ is also referenced in deploy.sh
+mkdir -p claims
+
+# deployment/ is also referenced in travis/build_and_deploy_container.sh
+mkdir -p deployment
+
 ./operator/plsync/mlabconfig.py \
     --format=scraper_kubernetes \
-    --template_input=deploy.yml \
+    --template_input=k8s/deploy.yml \
     --template_output=deployment/deploy-{{site_safe}}-{{node_safe}}-{{experiment_safe}}-{{rsync_module_safe}}.yml \
     --select="${PATTERN}"
 
-sed -e "s/{{GIGABYTES}}/${GIGABYTES}/" claim.yml > claim_template.yml
+sed -e "s/{{GIGABYTES}}/${GIGABYTES}/" k8s/claim.yml > k8s/claim_template.yml
 ./operator/plsync/mlabconfig.py \
     --format=scraper_kubernetes \
-    --template_input=claim_template.yml \
+    --template_input=k8s/claim_template.yml \
     --template_output=claims/claim-{{site_safe}}-{{node_safe}}-{{experiment_safe}}-{{rsync_module_safe}}.yml \
     --select="${PATTERN}"
